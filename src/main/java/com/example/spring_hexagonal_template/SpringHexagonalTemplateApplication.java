72816e6030716b894d775d6b02c6d19d81c01418
Dali <medalikebair@gmail.com>
2025-03-27 18:28:49 +0100

    Update README.md

---
+++ src/main/java/com/example/spring_hexagonal_template/SpringHexagonalTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_hexagonal_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonalTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHexagonalTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_hexagonal_template/SpringHexagonalTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_hexagonal_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHexagonalTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
